104cc198609fa3983053e3538e11c65bb8ae3328
zhangfeng <253524642@qq.com>
2018-03-03 22:28:52 +0800

    2018.03.03 Redis 缓存

--- src/main/java/cn/kotori/springboot/service/impl/CityServiceImpl.java
+++ src/main/java/cn/kotori/springboot/service/impl/CityServiceImpl.java
@@ -3,10 +3,14 @@ package cn.kotori.springboot.service.impl;
 import cn.kotori.springboot.dao.CityMapper;
 import cn.kotori.springboot.domain.City;
 import cn.kotori.springboot.service.CityService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 /**
  * 城市业务逻辑实现类
@@ -14,17 +18,41 @@ import java.util.List;
  * Created by bysocket on 07/02/2017.
  */
 @Service
+@Slf4j
 public class CityServiceImpl implements CityService {
 
     @Autowired
     private CityMapper cityMapper;
+
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     public List<City> findAllCity(){
         return cityMapper.selectAll();
     }
 
     public City findCityById(int id) {
-        return cityMapper.selectByPrimaryKey(id);
+        // 从缓存中获取城市信息
+        String key = "city_" + id;
+        ValueOperations<String, Object> operations = redisTemplate.opsForValue();
+
+        // 缓存存在
+        boolean hasKey = redisTemplate.hasKey(key);
+        if (hasKey) {
+            City city = (City) operations.get(key);
+
+            log.info("CityServiceImpl.findCityById() : 从缓存中获取了城市 >> " + city.toString());
+            return city;
+        }
+
+        // 从 DB 中获取城市信息
+        City city = cityMapper.selectByPrimaryKey(id);
+
+        // 插入缓存
+        operations.set(key, city, 100, TimeUnit.SECONDS);
+        log.info("CityServiceImpl.findCityById() : 城市插入缓存 >> " + city.toString());
+
+        return city;
     }
 
     public int saveCity(City city) {
