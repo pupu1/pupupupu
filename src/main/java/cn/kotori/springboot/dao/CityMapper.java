60b15160708a62648e5266d47e76cd5b5c5d963b
zhangfeng <253524642@qq.com>
2018-03-03 15:34:23 +0800

    2018.03.03 Mybatis v2.0

---
+++ src/main/java/cn/kotori/springboot/dao/CityMapper.java
@@ -0,0 +1,21 @@
+package cn.kotori.springboot.dao;
+
+import cn.kotori.springboot.domain.City;
+
+import java.util.List;
+
+public interface CityMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(City record);
+
+    int insertSelective(City record);
+
+    City selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(City record);
+
+    int updateByPrimaryKey(City record);
+
+    List<City> selectAll();
+}
